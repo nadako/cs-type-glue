dc772b861fe10ff3761103479450e8ba95e47cf1
Dan Korostelev <nadako@gmail.com>
2018-01-15 19:22:02 +0300

    C# playground project

---
+++ CS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using slapi.rambo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var gameData = new GameData(
+            heroId: new HeroId(10),
+            items: new GameData_items_element[0],
+            level: 15,
+            name: "Hi",
+            ready: true,
+            someItems: new Dictionary<SomeKey, Some>(),
+            state: StateId.Building);
+
+        Console.WriteLine(gameData.toStructure().ToString());
+        Console.ReadKey();
+    }
+}
