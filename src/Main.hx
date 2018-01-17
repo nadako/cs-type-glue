a49cc636c8d5129462c8bbd1c5b78ac75e932abe
Dan Korostelev <nadako@gmail.com>
2018-01-17 15:14:38 +0300

    minor

--- src/Main.hx
+++ src/Main.hx
@@ -1,11 +1,9 @@
 #if macro
 import haxe.macro.Context;
-using haxe.macro.ExprTools;
 
 class Main {
 	static function main() {
-		var helper = new nativetypes.Generator("slapi").generate(Context.getType("rambo.GameData"), Context.currentPos(), null);
-		trace(helper.generateConvertBackExpr(macro null).toString());
+		new nativetypes.Generator("slapi").generate(Context.getType("rambo.GameData"), Context.currentPos(), null);
 	}
 }
 #end
