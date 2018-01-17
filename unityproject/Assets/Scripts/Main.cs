2127f0076ea7eb52474da7a9eea804800816d75d
Dan Korostelev <nadako@gmail.com>
2018-01-17 20:43:15 +0300

    add actual unity project

---
+++ unityproject/Assets/Scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using slapi.rambo;
+using UniRx;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+	void Awake()
+    {
+        // можно создавать из C#
+		var p = new Player(null, null, null, StateId.Idle);
+
+        // реактив проперти вся хуйня
+        p.state.Subscribe(newState => Debug.Log("New state is: " + newState));
+
+        // диспетч из ШЛ дергает реактив проперти \o/
+        p.Dispatch(new Stack<string>(new [] {"state"}), "upgrading");
+	}
+}
